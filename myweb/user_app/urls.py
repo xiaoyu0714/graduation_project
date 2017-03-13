3c0a75594617e2c6144d4921162838a4c41c8bb2
xiaoyu0714 <416031666@qq.com>
2017-03-13 19:18:58 +0800

    requirement and bug fixed

--- myweb/user_app/urls.py
+++ myweb/user_app/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^index$', views.index, name='index'),
     url(r'^login$',views.login,name='login'),
-    url(r'^verify$',views.verify,name='verify')
+    url(r'^verify$',views.verify,name='verify'),
+    url(r'^sharethings$', views.sharethings, name='sharethings'),
+   	url(r'^things_can_be_borrowed$', views.things_can_be_borrowed, name='things_can_be_borrowed') 
 ]
--- myweb/user_app/views.py
+++ myweb/user_app/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 from .models import User
 # Create your views here.
 
-def index(request,user_name=None):
+def index(request):
     return render(request,'index.html')
 
 
 def login(request):
     return render(request,'login.html')
+    
 
 def verify(request):
     user = User.objects.all().filter(name=request.POST['name'],pwd=request.POST['pwd'])
@@ -16,3 +17,10 @@ def verify(request):
         return render(request,'user_home.html')
     else:
         return render(request,'login_fail.html')
+        
+def sharethings(request):
+	return render(request,'sharethings.html')
+	
+
+def things_can_be_borrowed(request):
+	return render(request,'things_can_be_borrowed.html')
