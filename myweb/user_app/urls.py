f655610c87e019bc3b85740146e5f9e055bb4df2
chowc <1398292615@qq.com>
2017-03-14 21:43:01 +0800

    add login middleware

--- myweb/user_app/urls.py
+++ myweb/user_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^login$',views.login,name='login'),
     url(r'^verify$',views.verify,name='verify'),
     url(r'^sharethings$', views.sharethings, name='sharethings'),
-   	url(r'^things_can_be_borrowed$', views.things_can_be_borrowed, name='things_can_be_borrowed') 
+   	url(r'^things_can_be_borrowed$', views.things_can_be_borrowed, name='things_can_be_borrowed')
 ]
