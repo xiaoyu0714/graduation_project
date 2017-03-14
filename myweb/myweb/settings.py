0fcf28680c29f7adc20071e62040a4b217c288dd
xiaoyu0714 <416031666@qq.com>
2017-03-14 23:14:44 +0800

    merge

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-	'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
